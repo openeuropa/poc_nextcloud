4a403a27699f6f9831aa0567cb93989a43a70a7f
Andreas Hennings <andreas@dqxtech.net>
2023-03-25 01:37:27 +0100

    OEC-893: Use patch --merge to be more tolerant to changed code or already applied patches.

--- resources/docker-image/build.patch.sh
+++ resources/docker-image/build.patch.sh
@@ -9,11 +9,11 @@ cd /usr/src/nextcloud/
 
 # Patch the user_cas app.
 echo "Patch: \OCP\App was removed."
-curl https://patch-diff.githubusercontent.com/raw/felixrupp/user_cas/pull/106.diff | patch -p1 -d custom_apps/user_cas
+curl https://patch-diff.githubusercontent.com/raw/felixrupp/user_cas/pull/106.diff | patch --merge -p1 -d custom_apps/user_cas
 echo "Patch: Don't pass NULL to setcookie(,,*)."
-curl https://patch-diff.githubusercontent.com/raw/felixrupp/user_cas/pull/108.diff | patch -p1 -d custom_apps/user_cas
+curl https://patch-diff.githubusercontent.com/raw/felixrupp/user_cas/pull/108.diff | patch --merge -p1 -d custom_apps/user_cas
 
 # Patch phpCAS to link to http instead of https.
 # This is needed to make it work with the cas mock server.
 # See https://github.com/felixrupp/user_cas/issues/109
-cat /image/phpCAS.http.patch | patch -p0 -d custom_apps/user_cas/vendor/jasig/phpcas
+cat /image/phpCAS.http.patch | patch --merge -p0 -d custom_apps/user_cas/vendor/jasig/phpcas
